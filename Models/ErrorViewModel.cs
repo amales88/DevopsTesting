d29d52a147f47d33efc53d00c3d15147e68fa51d
amales88 <amales88@outlook.com>
2025-03-05 10:21:14 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DevopsTesting.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
